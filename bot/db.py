17f0884cfbf598ded2ab47dae413d32e89fe050c
mironovmeow <71277890+mironovmeow@users.noreply.github.com>
2021-11-07 23:18:21 +0700

    add logs in db

--- bot/db.py
+++ bot/db.py
@@ -6,10 +6,13 @@ Module to working with database (sqlite3)
 import sqlite3
 import typing
 
+from vkbottle.modules import logger
+
 conn = sqlite3.connect('db.sqlite3')
 
 
 def create_table():
+    logger.debug("Create table")
     cur = conn.cursor()
     cur.execute("""CREATE TABLE users (
     vk_id INT PRIMARY KEY,
@@ -22,6 +25,7 @@ def create_table():
 
 
 def add_session(vk_id: int, diary_session: str) -> None:
+    logger.debug(f"Add session of user: @id{vk_id}")
     cur = conn.cursor()
     cur.execute("INSERT INTO users (vk_id, diary_session) VALUES (?, ?);", (vk_id, diary_session))
     conn.commit()
@@ -29,6 +33,7 @@ def add_session(vk_id: int, diary_session: str) -> None:
 
 
 def add_user(vk_id: int, login: str, password: str) -> None:
+    logger.debug(f"Add credentials of user: @id{vk_id}")
     cur = conn.cursor()
     cur.execute("INSERT INTO users (vk_id, login, password) VALUES (?, ?, ?);", (vk_id, login, password))
     conn.commit()
@@ -36,6 +41,7 @@ def add_user(vk_id: int, login: str, password: str) -> None:
 
 
 def get_user(vk_id: int) -> typing.Optional[str]:
+    logger.debug(f"Get user: @id{vk_id}")
     cur = conn.cursor()
     cur.execute("SELECT login, password FROM users WHERE vk_id = (?);", (vk_id,))
     user = cur.fetchall()
@@ -46,6 +52,7 @@ def get_user(vk_id: int) -> typing.Optional[str]:
 
 
 def get_users() -> typing.List[typing.Tuple[int, typing.Optional[str], typing.Optional[str], typing.Optional[str]]]:
+    logger.debug(f"Get all users")
     cur = conn.cursor()
     cur.execute("SELECT vk_id, diary_session, login, password FROM users")
     users = cur.fetchall()
@@ -56,6 +63,7 @@ def get_users() -> typing.List[typing.Tuple[int, typing.Optional[str], typing.Op
 
 
 def delete_user(vk_id: int) -> None:
+    logger.debug(f"Delete user: @id{vk_id}")
     cur = conn.cursor()
     cur.execute("DELETE FROM users WHERE vk_id = ?;", (vk_id,))
     conn.commit()
