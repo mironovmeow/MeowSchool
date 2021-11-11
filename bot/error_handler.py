b5fca7bcb6bc6f653a7c883ae4ab692578aeefc6
mironovmeow <71277890+mironovmeow@users.noreply.github.com>
2021-11-12 00:10:25 +0700

    Add vkbottle_error_handler

--- bot/error_handler.py
+++ bot/error_handler.py
@@ -1,6 +1,7 @@
 import traceback
 from typing import Tuple
 
+from aiohttp import ClientError
 from loguru import logger
 from vkbottle import ErrorHandler, VKAPIError
 from vkbottle.bot import Message
@@ -13,7 +14,7 @@ error_handler = ErrorHandler(redirect_arguments=True)
 
 
 @error_handler.register_error_handler(APIError)
-async def exc_message_handler_diary_api(e: APIError, m: Message):
+async def message_handler_diary_api(e: APIError, m: Message):
     if not e.resp.ok:
         if e.resp.status == 401:
             logger.info(f"Re-auth {m.peer_id}")
@@ -31,13 +32,13 @@ async def exc_message_handler_diary_api(e: APIError, m: Message):
 
 
 @error_handler.register_error_handler(VKAPIError)
-async def exc_message_handler_vk_api(e: VKAPIError, m: Message):
-    logger.warning(f"VKApi error {e}")
+async def message_handler_vk_api(e: VKAPIError, m: Message):
+    logger.warning(f"VKApi error {e.description} {e.code}")
     await m.answer("Временные неполадки с сервером. Повторите попытку позже")
 
 
 @error_handler.register_undefined_error_handler
-async def exc_message_handler(e: BaseException, m: Message):
+async def message_handler(e: BaseException, m: Message):
     logger.warning(f"Undefined error {e}")
     logger.error("\n" + traceback.format_exc())
     await m.answer("Временные неполадки с сервером. Повторите попытку позже")
@@ -47,7 +48,7 @@ callback_error_handler = ErrorHandler(redirect_arguments=True)
 
 
 @callback_error_handler.register_error_handler(APIError)
-async def exc_callback_handler_diary_api(e: APIError, event: MessageEvent):
+async def callback_handler_diary_api(e: APIError, event: MessageEvent):
     if not e.resp.ok:
         if e.resp.status == 401:
             logger.info(f"Re-auth {event.user_id}")
@@ -62,19 +63,19 @@ async def exc_callback_handler_diary_api(e: APIError, event: MessageEvent):
 
 
 @callback_error_handler.register_error_handler(VKAPIError[909])
-async def exc_callback_handler_vk_api(e: VKAPIError, event: MessageEvent):
-    logger.info(f"VKApi edit message error {e}")
+async def callback_handler_vk_api(e: VKAPIError, event: MessageEvent):
+    logger.info(f"VKApi edit message error {e.description} {e.code}")
     await event.show_snackbar("Сообщение слишком старое. Воспользуйтесь меню снизу")
 
 
 @callback_error_handler.register_error_handler(VKAPIError)
-async def exc_callback_handler_vk_api(e: VKAPIError, event: MessageEvent):
-    logger.warning(f"VKApi error {e}")
+async def callback_handler_vk_api(e: VKAPIError, event: MessageEvent):
+    logger.warning(f"VKApi error {e.description} {e.code}")
     await event.show_snackbar("Неизвестная ошибка с VK 0_o")
 
 
 @callback_error_handler.register_undefined_error_handler
-async def exc_callback_handler(e: BaseException, event: MessageEvent):
+async def callback_handler(e: BaseException, event: MessageEvent):
     logger.warning(f"Undefined error {e}")
     logger.error("\n" + traceback.format_exc())
     await event.show_snackbar("Неизвестная ошибка 0_o")
@@ -84,9 +85,22 @@ diary_date_error_handler = ErrorHandler(redirect_arguments=True)
 
 
 @diary_date_error_handler.register_error_handler(APIError)
-async def exc_diary_date_handler(e: APIError, m: Message, args: Tuple[str]):
+async def diary_date_handler(e: APIError, m: Message, args: Tuple[str]):
     if not e.json_success:
         logger.debug(f"Wrong date {await e.resp.json()} {args[0]}")
         await m.answer("Указана неверная дата. Попробуйте ещё раз")
     else:
-        await exc_message_handler_diary_api(e, m)
+        await message_handler_diary_api(e, m)
+
+
+vkbottle_error_handler = ErrorHandler()
+
+
+@vkbottle_error_handler.register_error_handler(ClientError)
+async def vkbottle_handler_aiohttp(e: ClientError):
+    logger.exception("aiohttp error. Again.")
+
+
+@vkbottle_error_handler.register_undefined_error_handler
+async def vkbottle_handler_aiohttp(e: BaseException):
+    logger.exception("UNDEFINED ERROR IN VKBOTTLE!!!")
--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,7 @@ from vkbottle.bot import Bot
 from vkbottle_callback import MessageEventLabeler
 
 from bot.blueprints import bp_list
+from bot.error_handler import vkbottle_error_handler
 
 if len(sys.argv) < 2:
     raise ValueError("Token is undefined")
@@ -12,7 +13,7 @@ TOKEN = sys.argv[1]
 
 labeler = MessageEventLabeler()
 
-bot = Bot(TOKEN, labeler=labeler)
+bot = Bot(TOKEN, labeler=labeler, error_handler=vkbottle_error_handler)
 
 
 for bp in bp_list:
